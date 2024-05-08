8a5797812accd304ba1967e6e372381a77605832
Shravankumar <shravankumarsayz@gmail.com>
2024-05-08 10:19:25 +0530

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -4,6 +4,8 @@ repo init -u https://github.com/StatiXOS/android_manifest.git -b udc-qpr2
 
 #Local manifests
 git clone https://github.com/shravansayz/local_manifests -b test .repo/local_manifests
+#repo sync
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 #build
 /opt/crave/resync.sh
