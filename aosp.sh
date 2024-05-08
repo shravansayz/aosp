c2320b8479fc6f50252f0fec6c038c05176f9db9
Shravankumar <shravankumarsayz@gmail.com>
2024-05-08 14:07:36 +0530

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -4,8 +4,12 @@ repo init -u https://github.com/StatiXOS/android_manifest.git -b udc-qpr2
 
 #Local manifests
 git clone https://github.com/shravansayz/local_manifests -b test .repo/local_manifests
+
 #repo sync
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+#ksu
+cd kernel/realme/sdm710 && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash - && cd ../../..
 
 #build
 /opt/crave/resync.sh
